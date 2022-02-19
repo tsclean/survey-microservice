31a848c1442a927597ae114353830e931e68ee92
John Piedrahita <citymanager02@gmail.com>
2022-02-18 23:42:45 -0500

    refactor: update prettier config global project

--- src/application/config/environment.ts
+++ src/application/config/environment.ts
@@ -1,15 +1,15 @@
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
-dotenv.config({ path: '.env' })
+dotenv.config({ path: '.env' });
 
 /**
 |----------------------------------------------------------------------------------------|
     App Configuration
 |----------------------------------------------------------------------------------------|
 */
-export const ENVIRONMENT = process.env.NODE_ENV
-const PROD = ENVIRONMENT === 'production'
-export const PORT = process.env.PORT
+export const ENVIRONMENT = process.env.NODE_ENV;
+const PROD = ENVIRONMENT === 'production';
+export const PORT = process.env.PORT;
 
 /**
 |----------------------------------------------------------------------------------------|
@@ -17,7 +17,7 @@ export const PORT = process.env.PORT
 |----------------------------------------------------------------------------------------|
 */
 
-export const SESSION_SECRET = process.env.JWT_SECRET || ''
+export const SESSION_SECRET = process.env.JWT_SECRET || '';
 
 /**
  * Use only if you include jwt
@@ -38,14 +38,14 @@ export const CONFIG_MYSQL = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DATABASE,
-}
+};
 
 /**
  *  Mongo DB
  */
 export const MONGODB_URI = PROD
   ? process.env.MONGO_PRODUCTION
-  : process.env.MONGO_DEVELOPMENT
+  : process.env.MONGO_DEVELOPMENT;
 
 /**
  * Postgres
@@ -56,4 +56,4 @@ export const CONFIG_POSTGRES = {
   database: process.env.DATABASE_POSTGRES,
   password: process.env.DB_PASSWORD_POSTGRES,
   port: 5432,
-}
+};
