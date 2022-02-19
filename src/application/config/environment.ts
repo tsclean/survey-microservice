9a87da0e3142e72147615e6bd874b79532fd895e
John Piedrahita <citymanager02@gmail.com>
2022-02-19 00:44:55 -0500

    refactor: change files prettier eslint

--- src/application/config/environment.ts
+++ src/application/config/environment.ts
@@ -1,15 +1,15 @@
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
 
-dotenv.config({ path: '.env' });
+dotenv.config({ path: '.env' })
 
 /**
 |----------------------------------------------------------------------------------------|
     App Configuration
 |----------------------------------------------------------------------------------------|
 */
-export const ENVIRONMENT = process.env.NODE_ENV;
-const PROD = ENVIRONMENT === 'production';
-export const PORT = process.env.PORT;
+export const ENVIRONMENT = process.env.NODE_ENV
+const PROD = ENVIRONMENT === 'production'
+export const PORT = process.env.PORT
 
 /**
 |----------------------------------------------------------------------------------------|
@@ -17,7 +17,7 @@ export const PORT = process.env.PORT;
 |----------------------------------------------------------------------------------------|
 */
 
-export const SESSION_SECRET = process.env.JWT_SECRET || '';
+export const SESSION_SECRET = process.env.JWT_SECRET || ''
 
 /**
  * Use only if you include jwt
@@ -37,15 +37,15 @@ export const CONFIG_MYSQL = {
   host: process.env.HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DATABASE,
-};
+  database: process.env.DATABASE
+}
 
 /**
  *  Mongo DB
  */
 export const MONGODB_URI = PROD
   ? process.env.MONGO_PRODUCTION
-  : process.env.MONGO_DEVELOPMENT;
+  : process.env.MONGO_DEVELOPMENT
 
 /**
  * Postgres
@@ -55,5 +55,5 @@ export const CONFIG_POSTGRES = {
   user: process.env.DB_USER_POSTGRES,
   database: process.env.DATABASE_POSTGRES,
   password: process.env.DB_PASSWORD_POSTGRES,
-  port: 5432,
-};
+  port: 5432
+}
--- src/infrastructure/entry-points/api/add-survey-controller.ts
+++ src/infrastructure/entry-points/api/add-survey-controller.ts
@@ -1,19 +1,19 @@
-import { Adapter, Mapping, Post } from '@tsclean/core';
+import { Adapter, Mapping, Post } from '@tsclean/core'
 import {
   ADD_SURVEY_SERVICE,
-  IAddSurveyService,
-} from '@/domain/use-cases/add-survey-service';
-import { AddSurveyParams } from '@/domain/models/survey';
+  IAddSurveyService
+} from '@/domain/use-cases/add-survey-service'
+import { AddSurveyParams } from '@/domain/models/survey'
 
 @Mapping('api/v1/add-survey')
 export class AddSurveyController {
-  constructor(
+  constructor (
     @Adapter(ADD_SURVEY_SERVICE)
     private readonly addSurveyService: IAddSurveyService
   ) {}
 
   @Post()
-  async addSurveyController(data: AddSurveyParams): Promise<void> {
-    await this.addSurveyService.save({ ...data, date: new Date() });
+  async addSurveyController (data: AddSurveyParams): Promise<void> {
+    await this.addSurveyService.save({ ...data, date: new Date() })
   }
 }
