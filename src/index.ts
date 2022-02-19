90a481fe894564070cf60e584cf08ae972268168
John Piedrahita <citymanager02@gmail.com>
2022-02-19 00:20:55 -0500

    refactor: change prettier global

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import 'module-alias/register';
+import 'module-alias/register'
 
-import helmet from 'helmet';
-import { connect } from 'mongoose';
-import { StartProjectInit } from '@tsclean/core';
+import helmet from 'helmet'
+import { connect } from 'mongoose'
+import { StartProjectInit } from '@tsclean/core'
 
-import { AppContainer } from '@/application/app';
-import { MONGODB_URI, PORT } from '@/application/config/environment';
+import { AppContainer } from '@/application/app'
+import { MONGODB_URI, PORT } from '@/application/config/environment'
 
 async function run(): Promise<void> {
-  await connect(MONGODB_URI);
-  console.log('DB Mongo connected');
-  const app = await StartProjectInit.create(AppContainer);
-  app.use(helmet());
-  await app.listen(PORT, () => console.log('Running on port: ' + PORT));
+  await connect(MONGODB_URI)
+  console.log('DB Mongo connected')
+  const app = await StartProjectInit.create(AppContainer)
+  app.use(helmet())
+  await app.listen(PORT, () => console.log('Running on port: ' + PORT))
 }
 
-void run().catch();
+void run().catch()
