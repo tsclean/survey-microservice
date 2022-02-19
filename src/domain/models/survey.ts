815f7e2b75361348bdb1d333ba21180332841878
John Piedrahita <citymanager02@gmail.com>
2022-02-18 23:01:36 -0500

    feat: add survey entity

---
+++ src/domain/models/survey.ts
@@ -0,0 +1,14 @@
+export type SurveyModel = {
+  id: string;
+  question: string;
+  answers: SurveyAnswerModel[];
+  date: Date;
+  didAnswer?: boolean;
+};
+
+export type SurveyAnswerModel = {
+  image?: string;
+  answer: string;
+};
+
+export type AddSurveyParams = Omit<SurveyModel, 'id'>;
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import "module-alias/register";
+import 'module-alias/register';
 
 import helmet from 'helmet';
-import {StartProjectInit} from "@tsclean/core";
-        
-import {AppContainer} from "@/application/app";
-import {PORT} from "@/application/config/environment";
-    
-async function init() {
-    const app = await StartProjectInit.create(AppContainer)
-    app.use(helmet());
-    await app.listen(PORT, () => console.log('Running on port ' + PORT))
+import { StartProjectInit } from '@tsclean/core';
+
+import { AppContainer } from '@/application/app';
+import { PORT } from '@/application/config/environment';
+
+async function init(): Promise<void> {
+  const app = await StartProjectInit.create(AppContainer);
+  app.use(helmet());
+  await app.listen(PORT, () => console.log('Running on port ' + PORT));
 }
-   
-init().catch();
+
+void init().catch((err) => console.log(err));
