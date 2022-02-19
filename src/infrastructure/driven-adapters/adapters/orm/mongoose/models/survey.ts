f0a7038a182914302e61afe8b04416b4ff065d86
John Piedrahita <citymanager02@gmail.com>
2022-02-19 00:05:22 -0500

    feat: add orm adapter survey

---
+++ src/infrastructure/driven-adapters/adapters/orm/mongoose/models/survey.ts
@@ -0,0 +1,27 @@
+import { SurveyModel } from '@/domain/models/survey';
+import { model, Schema } from 'mongoose';
+
+const schema = new Schema<SurveyModel>({
+  id: {
+    type: String,
+  },
+  question: {
+    type: String,
+    required: true,
+  },
+  answers: {
+    required: true,
+    type: [
+      {
+        image: String,
+        answer: String,
+      },
+    ],
+  },
+  date: Date.now(),
+  didAnswer: {
+    type: Boolean,
+  },
+});
+
+export const SurveyModelSchema = model<SurveyModel>('surveys', schema);
