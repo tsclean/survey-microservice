d641ddb52073f45d94ffe2546b3404cc5f2ce12f
John Piedrahita <citymanager02@gmail.com>
2022-02-21 16:38:40 -0500

    feat: add load survey by id controller

---
+++ src/infrastructure/entry-points/api/load-survey-by-id-controller.ts
@@ -0,0 +1,16 @@
+import { Mapping, Get, Param, Adapter } from '@tsclean/core'
+import { ILoadSurveyByIdService, LOAD_SURVEY_BY_ID_SERVICE } from '@/domain/use-cases/load-survey-by-id-service'
+
+@Mapping('api/v1/load-survey-by-id')
+export class LoadSurveyByIdController {
+  constructor (
+    @Adapter(LOAD_SURVEY_BY_ID_SERVICE)
+    private readonly loadSurveyByIdService: ILoadSurveyByIdService
+  ) {
+  }
+
+  @Get(':id')
+  async loadSurveyByIdController (@Param() id: string): Promise<any> {
+    return await this.loadSurveyByIdService.loadById(id)
+  }
+}
