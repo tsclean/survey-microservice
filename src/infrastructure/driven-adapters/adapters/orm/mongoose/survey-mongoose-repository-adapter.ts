0269cbed4e3ff5fce072d10f606555675c3f5cf8
John Piedrahita <citymanager02@gmail.com>
2022-02-21 07:27:56 -0500

    feat: add load surveys mongoose adapter

--- src/infrastructure/driven-adapters/adapters/orm/mongoose/survey-mongoose-repository-adapter.ts
+++ src/infrastructure/driven-adapters/adapters/orm/mongoose/survey-mongoose-repository-adapter.ts
@@ -1,9 +1,14 @@
-import { AddSurveyParams } from '@/domain/models/survey'
+import { AddSurveyParams, SurveyModel } from '@/domain/models/survey'
 import { SurveyModelSchema as Model } from '@/infrastructure/driven-adapters/adapters/orm/mongoose/models/survey'
 import { IAddSurveyRepository } from '@/domain/models/contracts/add-survey-repository'
+import { ILoadSurveysRepository } from '@/domain/models/contracts/load-surveys-repository'
 
-export class SurveyMongooseRepositoryAdapter implements IAddSurveyRepository {
+export class SurveyMongooseRepositoryAdapter implements IAddSurveyRepository, ILoadSurveysRepository {
   async save (data: AddSurveyParams): Promise<void> {
     await Model.create(data)
+  }
+
+  async load (accountId: string): Promise<ILoadSurveysRepository.Result> {
+    return Model.findById<SurveyModel[]>(accountId)
   }
 }
